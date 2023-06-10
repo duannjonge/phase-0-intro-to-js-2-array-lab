b78ec02f1392bc965e071f7e7f365fbb2ce45780
duan spot <brandennjonge@gmail.com>
2023-06-10 14:21:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats=[ "Milo", "Otis", "Garfield"];
+// cats.push("Ralph")
+function destructivelyAppendCat(name)
+{
+cats.push("Ralph");
+
+}
+function destructivelyRemoveLastCat(){
+
+    cats.pop();
+}
+function destructivelyPrependCat(name)
+{
+
+    cats.unshift("Bob");
+}
+function destructivelyRemoveFirstCat(){
+
+
+    cats.shift();
+}
+function appendCat(name){
+
+    return [...cats, "Broom"];
+}
+function prependCat(name)
+{
+
+    return ["Arnold", ...cats];
+}
+// function  removeLastCat()
+// {
+
+//    return cats.slice(-1) ;
+// }
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
+  
+
